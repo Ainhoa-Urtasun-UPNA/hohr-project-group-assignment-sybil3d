ba9c2c01dc535720a3092f7ea5f8f3354154d04b
Arcos127037 <arcos.127037@e.unavarra.es>
2021-03-08 15:07:36 +0000

    .

--- HRplanning.py
+++ HRplanning.py
@@ -1,9 +1,9 @@
 import numpy
 
 # Forecasting number of available workers
-D = numpy.array([[],[],[]])
-M = numpy.array([[],[],[]])
-b = numpy.array([[],[],[]])
+D = numpy.array([[15,0,0],[0,20,0],[0,0,50]])
+M = numpy.array([[5,2,0],[0,10,0],[5,5,45]])
+b = numpy.array([[4],[0],[0]])
 a = M@numpy.linalg.inv(D)@b
 print(a)
 
